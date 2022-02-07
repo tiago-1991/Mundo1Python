636fd8426b100173a05fe841da8bd49a6a049711
tiago-1991 <53984116+tiago-1991@users.noreply.github.com>
2022-02-07 03:11:33 -0300

    Delete aumentoSalario.py

--- Mundo1/aumentoSalario.py
+++
@@ -1,8 +0,0 @@
-salario = float(input('Digite o salário atual do Funcionário:'))
-aumento = (salario)+salario*0.15
-
-print('O salário do funcionário passou de R${} para R${}\nDevido ao aumento de 15%'.format(salario,aumento))
-
-
-
-
