b7dc69d7b9bb23dd78c44553403adfa9cfb52d5f
tiago-1991 <53984116+tiago-1991@users.noreply.github.com>
2022-02-07 03:10:00 -0300

    Delete aluguelCarro.py

--- Mundo1/aluguelCarro.py
+++
@@ -1,9 +0,0 @@
-km = float(input('Digite a quantidade de Km rodados pelo carro:'))
-dias = int(input('Digite a quantidade de dias em que o carro ficou alugado:'))
-
-calcKm = km*0.15
-calcDias = dias*60
-aPagar = calcKm+calcDias
-
-print('Valor Kilometragem = R${:.2f}\nValor por dias alugados = R${:.2f}\nValor Total a Pagar = R${:.2f}'
-.format(calcKm,calcDias,aPagar))
