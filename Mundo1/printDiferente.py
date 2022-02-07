86c3c0c8d8f82ce22cd2f96e64c0fe9cddbda99c
tiago-1991 <53984116+tiago-1991@users.noreply.github.com>
2022-02-07 03:15:47 -0300

    Delete printDiferente.py

--- Mundo1/printDiferente.py
+++
@@ -1,5 +0,0 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-nome = input ('Qual é seu nome?')
-print('Prazer em te conhecer {:=^20}!'.format(nome))
-#nome armazenado na variável centralizado(^) em um espaço de 20 caracteres com sinal de igual: =
